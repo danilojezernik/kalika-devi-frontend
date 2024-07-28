2470d00847c9cd27eab7a88e6f6221ebe44cd374
Danilo Jezernik <dani.jezernik@gmail.com>
2024-07-28 13:32:30 +0200

    Second commit: Cleaning data, adding axios,...

---
+++ src/components/Home.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<h1 class="text-2xl">Home</h1>
+</template>
+
+<style scoped>
+
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
