1bb6423b20ea9026f1e4d88d9c7e4dbacca51f64
Danilo Jezernik <dani.jezernik@gmail.com>
2024-08-06 10:25:11 +0200

    Configuration for tsconfig.json

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,6 @@ import Home from '@/core/pages/public/Home/Home.vue'
 import AboutKalika from '@/core/pages/public/Kalika/AboutKalika.vue'
 import Blog from '@/core/pages/public/Blog/Blog.vue'
 import NotFound from '@/core/pages/public/404/NotFound.vue'
-import BlogId from '@/core/pages/public/BlogId/BlogId.vue'
 import AboutMe from '@/core/pages/public/AboutMe/AboutMe.vue'
 import Login from '@/core/pages/public/Login/Login.vue'
 
@@ -13,6 +12,7 @@ import Login from '@/core/pages/public/Login/Login.vue'
 import Admin from '@/core/pages/private/Admin/Admin.vue'
 import BlogAdmin from '@/core/pages/private/Blog/All/BlogAdmin.vue'
 import Contact from '@/core/pages/public/Contact/Contact.vue'
+import BlogId from '@/core/pages/public/BlogId/BlogId.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
