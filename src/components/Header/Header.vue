cdf94eaf13b09f086ebb2b0c10c41b09328221b4
Danilo Jezernik <dani.jezernik@gmail.com>
2024-08-06 10:49:27 +0200

    Changes to arhitecture

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,8 +1,2 @@
-<script setup lang="ts">
-import { RouterLink } from 'vue-router'
-import { HEADER } from '@/shared/global-const/global.const'
-</script>
-
-<template>
-  <RouterLink v-for="links in HEADER" :class="links.class" :to="links.to">{{ links.title }}</RouterLink>
-</template>
+<template src="./header.html"></template>
+<script src="./header.ts"></script>
---
+++ src/components/Header/header.ts
@@ -0,0 +1,11 @@
+import { defineComponent } from "vue";
+import { HEADER } from '@/shared/global-const/global.const'
+
+export default defineComponent({
+  setup() {
+
+    return {
+      HEADER
+    }
+  },
+})
